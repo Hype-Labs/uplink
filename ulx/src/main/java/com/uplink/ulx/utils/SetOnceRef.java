5e5c7a98906c716f055dbb5cdb57616e8e0f4681
o.rabinovych <optisamit@gmail.com>
2022-04-09 15:51:50 +0300

    Improve thread-safety of SetOnceRef

--- ulx/src/main/java/com/uplink/ulx/utils/SetOnceRef.java
+++ ulx/src/main/java/com/uplink/ulx/utils/SetOnceRef.java
@@ -6,9 +6,11 @@ import androidx.annotation.NonNull;
  * Holder class that allows to set a reference to {@link T} only once. This is useful when we need to
  * ensure that a certain field is not modified after initialization,
  * which can occur after enclosing class is constructed
+ *
+ * The class is not thread-safe, but is safe to publish after {@link #setRef(T)} is called
  */
 public class SetOnceRef<T> {
-   private T ref;
+   private volatile T ref;
 
    @NonNull
    public T getRef() {
