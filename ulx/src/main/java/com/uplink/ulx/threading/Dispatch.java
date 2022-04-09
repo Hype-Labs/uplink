da0c69f51ab7f928638074745dfb62eeef16b9d8
o.rabinovych <optisamit@gmail.com>
2022-04-09 16:14:02 +0300

    Make Dispatch thread-safe

--- ulx/src/main/java/com/uplink/ulx/threading/Dispatch.java
+++ ulx/src/main/java/com/uplink/ulx/threading/Dispatch.java
@@ -5,19 +5,12 @@ import android.os.Looper;
 
 public class Dispatch {
 
-    private static Handler mainHandler;
+    private static final Handler mainHandler = new Handler(Looper.getMainLooper());
 
     private Dispatch() {
     }
 
-    private static Handler getMainHandler() {
-        if (Dispatch.mainHandler == null) {
-            Dispatch.mainHandler = new Handler(Looper.getMainLooper());
-        }
-        return Dispatch.mainHandler;
-    }
-
     public static void post(Runnable runnable) {
-        getMainHandler().post(runnable);
+        mainHandler.post(runnable);
     }
 }
