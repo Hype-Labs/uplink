f92d44035ef8d991c7cb2a2e37f4dadfb67a85ea
o.rabinovych <optisamit@gmail.com>
2022-04-24 19:54:12 +0300

    Add timeout param to SerialOperationsManager.enqueue()

--- ulx/src/main/java/com/uplink/ulx/utils/SerialOperationsManager.java
+++ ulx/src/main/java/com/uplink/ulx/utils/SerialOperationsManager.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import timber.log.Timber;
 
@@ -72,58 +73,85 @@ public class SerialOperationsManager {
      * run at all. {@link Completable#markAsComplete()} is idempotent
      */
     public Completable enqueue(Task task) {
-        final CountDownLatch latch = new CountDownLatch(1);
+        return enqueue(task, 0);
+    }
+
+    /**
+     * Enqueue a new task for execution.
+     *
+     * @param task      task to execute. It doesn't need to complete all of the work during its
+     *                  {@link Task#run(Completable)} method. The task is considered to be complete
+     *                  only after the {@link Completable#markAsComplete()} is called
+     * @param timeoutMs timeout, during which {@link Completable#markAsComplete()} is expected to be
+     *                  called. Countdown starts after {@link Task#run(Completable)} exits. A value
+     *                  of zero means no timeout
+     * @return {@link Completable} for marking the task as complete. It is the same object that will
+     * be passed to {@link Task#run(Completable)}. {@link Completable#markAsComplete()} can be
+     * called at any point - before, during or after {@link Task#run(Completable)} is called.
+     * Although it usually doesn't make sense, completing before the task is run will make it not
+     * run at all. {@link Completable#markAsComplete()} is idempotent
+     */
+    public Completable enqueue(Task task, int timeoutMs) {
+        final CountDownLatch completionLatch = new CountDownLatch(1);
         // Calling markComplete() will countdown the latch, thus allowing the queue to proceed
-        final Completable completable = latch::countDown;
-        try {
-            queueExecutor.execute(() -> {
-                final FutureTask<Void> future = new FutureTask<>(
-                        () -> {
-                            if (latch.getCount() > 0) {
-                                task.run(completable);
-                            } // else the task is already complete
-                        },
-                        null
-                );
+        final Completable completable = completionLatch::countDown;
+
+        final Runnable operationRunner = () -> {
+
+            final FutureTask<Void> future = new FutureTask<>(
+                    () -> {
+                        if (completionLatch.getCount() > 0) {
+                            task.run(completable);
+                        } // else the task is already complete
+                    },
+                    null
+            );
+
+            try {
+                // Submit the task to the operations executor
+                operationsExecutor.execute(future);
+            } catch (RejectedExecutionException e) {
+                Timber.w(e, "Failed to enqueue operation");
+                // Do not try waiting for the task - it will never complete
+                return;
+            }
+
+            try {
                 try {
-                    // Submit the task to the operations executor
-                    operationsExecutor.execute(future);
-                } catch (RejectedExecutionException e) {
-                    Timber.w(e, "Failed to enqueue operation");
-                    // Do not try waiting for the task - it will never complete
-                    return;
-                }
-                try {
-                    // Wait for the task to finish exit its run() method
+                    // Wait for the operation to run
                     future.get();
                 } catch (ExecutionException e) {
                     Timber.w(e, "Failed to execute operation");
                     // Do not wait for the completion flag - the task has failed
                     return;
-                } catch (InterruptedException e) {
-                    Timber.d(
-                            e,
-                            "Queue executor's thread was interrupted while waiting for an operation to run"
-                    );
-                    // Try to cancel the task
-                    future.cancel(true);
-                    // Reset the interruption flag
-                    Thread.currentThread().interrupt();
-                    // Do not wait for the completion flag
-                    return;
                 }
-                try {
-                    // Wait for the completion flag to be set
-                    latch.await();
-                } catch (InterruptedException e) {
-                    Timber.d(
-                            e,
-                            "Queue executor's thread was interrupted while waiting for an operation to complete"
-                    );
-                    // Reset the interruption flag
-                    Thread.currentThread().interrupt();
+
+                // Wait for the completion flag to be set
+                final boolean isTimeout;
+                if (timeoutMs > 0) {
+                    // No timeout - wait as long as needed
+                    completionLatch.await();
+                    isTimeout = false;
+                } else if (isTimeout = !completionLatch.await(timeoutMs, TimeUnit.MILLISECONDS)) {
+                    Timber.i("Timeout was hit while waiting for the operation's completion");
                 }
-            });
+
+                operationsExecutor.execute(() -> task.onComplete(isTimeout));
+
+            } catch (InterruptedException e) {
+                // This should only happen if destroy() was called. So we won't wait for the
+                // operation to complete anymore
+                Timber.d(e, "Queue executor's thread was interrupted");
+
+                future.cancel(true);
+
+                // Reset the interruption flag
+                Thread.currentThread().interrupt();
+            }
+        };
+
+        try {
+            queueExecutor.execute(operationRunner);
         } catch (RejectedExecutionException e) {
             Timber.w(
                     e,
@@ -153,5 +181,14 @@ public class SerialOperationsManager {
          *                    task is finished. This does not need to happen inside the method
          */
         void run(Completable completable);
+
+        /**
+         * Called when the task is complete. The call is submitted to the executor passed to the
+         * {@link SerialOperationsManager}'s constructor
+         *
+         * @param isTimeout whether the completion is by timeout
+         */
+        default void onComplete(boolean isTimeout) {
+        }
     }
 }
